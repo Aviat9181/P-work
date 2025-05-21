4fb8dffc8500bfa52c27572ca45db29f86ca0f1e
Aviat9181 <ilnurrz116@gmail.com>
2025-05-21 15:43:33 +0300

    Добавлено логирование

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import requests as rq
-
+from datetime import datetime
 
 def init(team_id):
     global id_score
@@ -12,12 +12,44 @@ def init(team_id):
         id_score[team_id]['losses'] = 0
 
 def check_correct(resp):
-    status=resp.status_code
-    if status>=300:
-        print(status,'Error')
-        if status==401:
-            print("Maybe token is invalid or empty")
-        exit(0)
+    if not isinstance(resp, rq.Response):
+        print("Error. Maybe, server returned incorrect answer or bad connection")
+    else:
+        status=resp.status_code
+        if status>=300:
+            print(status,'Error')
+            if status==401:
+                print("Maybe token is invalid or empty")
+
+            with open('logs.json','r') as f:
+                log=json.load(f)
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            error_data={"type":"response error","status code":status,"time":current_time}
+            log.append(error_data)
+            with open('logs.json','w') as f:
+                json.dump(log,f,indent=4)
+            exit(0)
+
+def add_question_to_logs(question,data,answer):
+    with open('logs.json','r') as f:
+        log=json.load(f)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    question_data={"type":"user question","data":data,"answer":answer,"time":current_time}
+    log.append(question_data)
+    with open('logs.json','w') as f:
+        json.dump(log,f,indent=4)
+
+def add_start_action_to_logs():
+    with open('logs.json','r') as f:
+        log=json.load(f)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    start_data={"type":"start of programm","time":current_time}
+    log.append(start_data)
+    with open('logs.json','w') as f:
+        json.dump(log,f,indent=4)
+
+
+add_start_action_to_logs()
 
 token_map = json.load(open("Token.json"))
 token = token_map["Token"]
@@ -45,10 +77,10 @@ for cur in teams.json():
     for pl_id in cur['players']:
         players_id.append(pl_id)
 
-# cnt = 0
+cnt = 0
 for pl_id in players_id:
-    # cnt += 1
-    # if cnt >= 3: break
+    cnt += 1
+    if cnt >= 3: break
     url = 'https://lksh-enter.ru/players/' + str(pl_id)
     player = rq.get(url, headers=header)
     check_correct(player)
@@ -99,12 +131,19 @@ while True:
         name = question_str[pos:len(question_str) - 1]
 
         if not name in team_to_id.keys():
+            add_question_to_logs(question[0],name,[0,0,0])
             print(0, 0, 0)
             continue
+
         team_id = team_to_id[name]
         wins = id_score[team_id]['wins']
         losses = id_score[team_id]['losses']
         delta = id_score[team_id]['delta']
+
+        ans={'wins':wins,'losses':losses,'delta':delta}
+        data={'name':name}
+        add_question_to_logs(question[0],data,ans)
+
         print(wins, losses, delta)
     elif question[0] == 'versus?':
         id_1 = int(question[1])
@@ -123,4 +162,10 @@ while True:
                 answer += 1
             elif team1_id2 and team2_id1:
                 answer += 1
+        
+        data={'id_1':id_1,'id_2':id_2}
+        add_question_to_logs(question[0],data,{'answer':answer})
         print(answer)
+    else:
+        add_question_to_logs(question[0],"incorrect question","incorrect question")
+        print("incorrect question")
